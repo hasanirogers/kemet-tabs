753d2c3a0ea4d6fc77552665b02a017965dac2d1
hasanirogers <hasani.rogers@gmail.com>
2020-01-15 15:41:12 -0500

    added select by index support

--- src/KemetTabs.js
+++ src/KemetTabs.js
@@ -9,6 +9,10 @@ export class KemetTabs extends LitElement {
         type: String,
         reflect: true,
       },
+
+      'selectedIndex': {
+        type: Number,
+      }
     };
   }
 
@@ -17,6 +21,7 @@ export class KemetTabs extends LitElement {
 
     this.tabs = [];
     this.panels = [];
+    this.selectedIndex = 0;
     this.addEventListener('kemet-tab-selected', this.tabSelectedChange.bind(this));
   }
 
@@ -43,39 +48,82 @@ export class KemetTabs extends LitElement {
   init() {
     const tabs = this.querySelectorAll('kemet-tab');
     const panels = this.querySelectorAll('kemet-tab-panel');
+    const index = {
+      tabs: 0,
+      panels: 0,
+    };
 
-    Array.prototype.forEach.call(tabs, tab => {
+    tabs.forEach(tab => {
+      // give each tab an index to select by if link/panel is not used
+      tab.index = index.tabs;
+      index.tabs += 1;
+
+      // store the list of tabs
       this.tabs = this.tabs.concat(tab);
     });
 
-    Array.prototype.forEach.call(panels, panel => {
+    panels.forEach(panel => {
+      // give each panel an index to select by if link/panel is not used
+      panel.index = index.panels;
+      index.panels += 1;
+
+      // store the list of panels
       this.panels = this.panels.concat(panel);
     });
+
+    this.tabs[1].selected = true;
+    this.panels[1].selected = true;
   }
 
   selectTab() {
-    Array.prototype.forEach.call(this.tabs, tab => {
-      const tabName = tab.getAttribute('link');
+    // if the user uses the selected attribute select by it
+    if(this.selected) {
+      this.tabs.forEach(tab => {
+        const tabName = tab.getAttribute('link');
 
-      if (tabName === this.selected) {
-        tab.selected = true;
-        this.selected = tabName;
-      } else {
-        tab.selected = false;
-      }
-    });
+        if (tabName === this.selected) {
+          tab.selected = true;
+          this.selected = tabName;
+        } else {
+          tab.selected = false;
+        }
+      });
+
+    // otherwise select by index
+    } else {
+      this.tabs.forEach(tab => {
+        if(this.selectedIndex === tab.index) {
+          tab.selected = true;
+        } else {
+          tab.selected = false;
+        }
+      });
+    }
   }
 
   selectPanel() {
-    Array.prototype.forEach.call(this.panels, panel => {
-      const panelName = panel.getAttribute('panel');
+    // if the user uses the selected attribute select by it
+    if(this.selected) {
+      this.panels.forEach(panel => {
+        const panelName = panel.getAttribute('panel');
 
-      if (panelName === this.selected) {
-        panel.selected = true;
-      } else {
-        panel.selected = false;
-      }
-    });
+        if (panelName === this.selected) {
+          panel.selected = true;
+        } else {
+          panel.selected = false;
+        }
+      });
+
+    // otherwise select by index
+    } else {
+      this.panels.forEach(panel => {
+        if(this.selectedIndex === panel.index) {
+          panel.selected = true;
+        } else {
+          panel.selected = false;
+        }
+      });
+    }
   }
 
   dispatchTabChange() {
@@ -89,7 +137,15 @@ export class KemetTabs extends LitElement {
   }
 
   tabSelectedChange(event) {
-    this.selected = event.detail.getAttribute('link');
+    // if selected has been set, use the string attribute
+    if (this.selected) {
+      this.selected = event.detail.getAttribute('link');
+
+    // otherwise use the generated index
+    } else {
+      this.selectedIndex = event.detail.index;
+    }
+
     this.dispatchTabChange();
   }
 }
